2e557bea0f9e0ada8ce183c3e2562e658387ca54
Archivicore <82773834+Archivicore@users.noreply.github.com>
2021-07-21 13:02:08 +0700

    rebuild help.py

--- Archx/plugins/help.py
+++ Archx/plugins/help.py
@@ -472,3 +472,125 @@ if Archx.has_bot:
                     )
 
         await inline_query.answer(results=results, cache_time=3)
+
+
+    @Archx.bot.on_inline_query()
+    async def inline_query_handler(client, query):
+        try:
+            text = query.query.strip().lower()
+            answers = []
+            if text.strip() == "":
+                answerss = await inline_help_func(__HELP__)
+                await client.answer_inline_query(
+                    query.id, results=answerss, cache_time=10
+                )
+                return
+            elif text.split()[0] == "tr":
+                if len(text.split()) < 3:
+                    return await client.answer_inline_query(
+                        query.id,
+                        results=answers,
+                        switch_pm_text="Translator | tr [LANG] [TEXT]",
+                        switch_pm_parameter="inline",
+                    )
+                lang = text.split()[1]
+                tex = text.split(None, 2)[2].strip()
+                answerss = await translate_func(answers, lang, tex)
+                await client.answer_inline_query(
+                    query.id,
+                    results=answerss,
+                )
+
+            elif text.split()[0] == "google":
+                if len(text.split()) < 2:
+                    return await client.answer_inline_query(
+                        query.id,
+                        results=answers,
+                        switch_pm_text="Google Search | google [QUERY]",
+                        switch_pm_parameter="inline",
+                    )
+                tex = text.split(None, 1)[1].strip()
+                answerss = await google_search_func(answers, tex)
+                await client.answer_inline_query(
+                    query.id,
+                    results=answerss,
+                )
+
+            elif text.split()[0] == "yt":
+                if len(text.split()) < 2:
+                    return await client.answer_inline_query(
+                        query.id,
+                        results=answers,
+                        switch_pm_text="YouTube Search | yt [QUERY]",
+                        switch_pm_parameter="inline",
+                    )
+                tex = text.split(None, 1)[1].strip()
+                answerss = await youtube_func(answers, tex)
+                await client.answer_inline_query(query.id, results=answerss)
+
+            elif text.split()[0] == "lyrics":
+                if len(text.split()) < 2:
+                    return await client.answer_inline_query(
+                        query.id,
+                        results=answers,
+                        switch_pm_text="Lyrics Search | lyrics [QUERY]",
+                        switch_pm_parameter="inline",
+                    )
+                tex = text.split(None, 1)[1].strip()
+                answerss = await lyrics_func(answers, tex)
+                await client.answer_inline_query(query.id, results=answerss)
+
+            elif text.split()[0] == "search":
+                if len(text.split()) < 2:
+                    return await client.answer_inline_query(
+                        query.id,
+                        results=answers,
+                        switch_pm_text="Global Message Search. | search [QUERY]",
+                        switch_pm_parameter="inline",
+                    )
+
+            elif text.split()[0] == "ytmusic":
+                if len(text.split()) < 2:
+                    return await client.answer_inline_query(
+                        query.id,
+                        results=answers,
+                        switch_pm_text="YT Music | ytmusic [url]",
+                        switch_pm_parameter="inline",
+                    )
+                tex = query.query.split(None, 1)[1].strip()
+                answerss = await yt_music_func(answers, tex)
+                await client.answer_inline_query(
+                    query.id, results=answerss, cache_time=2
+                )
+
+            elif text.split()[0] == "info":
+                if len(text.split()) < 2:
+                    return await client.answer_inline_query(
+                        query.id,
+                        results=answers,
+                        switch_pm_text="User Info | info [USERNAME|ID]",
+                        switch_pm_parameter="inline",
+                    )
+                tex = text.split()[1].strip()
+                answerss = await user_info_inline_func(answers, tex)
+                await client.answer_inline_query(
+                    query.id, results=answerss, cache_time=2
+                )
+
+            elif text.split()[0] == "chat_info":
+                if len(text.split()) < 2:
+                    return await client.answer_inline_query(
+                        query.id,
+                        results=answers,
+                        switch_pm_text="Chat Info | chat_info [USERNAME|ID]",
+                        switch_pm_parameter="inline",
+                    )
+                tex = text.split()[1].strip()
+                answerss = await chat_info_inline_func(answers, tex)
+                await client.answer_inline_query(
+                    query.id, results=answerss, cache_time=2
+                )
+
+        except Exception as e:
+            e = traceback.format_exc()
+            print(e, " InLine")
